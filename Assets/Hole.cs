c2b72e72e45aa222f0dcc70ca04bff53a1fc3afd
Petrasuc Ana <petrasucana@yahoo.com>
2024-10-16 18:32:24 -0700

    ScoreManager

---
+++ Assets/Hole.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Hole : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ball")) 
+        {
+            FindObjectOfType<ScoreManager>().IncrementScore(); 
+
+            Debug.Log("Score incremented");
+
+            //resetam pozitia mingii:
+        }
+    }
+}
